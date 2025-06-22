9454abd2cf961b6ed1624e85020a3bb8535a4478
Michał Gliwiński <gliwinskmich@gmail.com>
2025-06-22 21:52:21 +0200

    Zad. 4 skrypt

---
+++ 4._Dodawanie_poprawek.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+#sprawdzenie czy docelowe pliki istnieją
+sprawdz_pliki() {
+    if [ ! -f "lista.txt" ]; then
+        echo "Błąd: brak pliku 'lista.txt'"
+        exit 1
+    fi
+
+    if [ ! -f "lista-pop.txt" ]; then
+        echo "Błąd: brak pliku 'lista-pop.txt'"
+        exit 1
+    fi
+}
+
+#porównanie zawartości plików
+porownaj_pliki() {
+    echo -e "\n\nRóżnice między pierwotną listą a listą z poprawkami:"
+    echo -e "\n> słowa występujące wyłącznie w pierwotnej liście (lista.txt):"
+    comm -23 <(sort lista.txt) <(sort lista-pop.txt)
+    echo -e "\n> słowa występujące wyłącznie w zaktualizowanej liście (lista-pop.txt):"
+    comm -13 <(sort lista.txt) <(sort lista-pop.txt)
+    echo ""
+}
+
+# Funkcja aktualizująca plik
+aktualizuj_plik() {
+    read -p "Czy zaktualizować treść listy (lista.txt) poprawkami (lista-pop.txt)? [T/N]: " odpowiedz
+    if [[ "$odpowiedz" =~ [Tt] ]]; then
+        cp -f lista-pop.txt lista.txt
+        echo -e "\n> lista została zaktualizowana.\n  Aby w przyszłości ręcznie aktualizować listę, należy wykonać polecenie:\n  $ cp lista-pop.txt lista.txt"
+        else
+        echo -e "\n> anulowano aktualizację, lista pozostaje bez zmian.\n  Aby ręcznie zaktualizować listę, należy wykonać polecenie:\n  $ cp lista-pop.txt lista.txt"
+        echo ""
+        echo -e "########## Operacja zakończona ##########"
+        echo ""
+        exit 0
+    fi
+}
+
+# Funkcja weryfikująca sumy MD5
+weryfikuj_md5() {
+    echo -e "\nWeryfikacja sum kontrolnych (MD5):"
+    echo ""
+
+    md5_lista=$(md5sum lista.txt | awk '{print $1}')
+    md5_pop=$(md5sum lista-pop.txt | awk '{print $1}')
+
+    echo "> lista po aktualizacji: $md5_lista"
+    echo "> lista poprawek: $md5_pop"
+
+    if [ "$md5_lista" == "$md5_pop" ]; then
+        echo -e "\nSumy MD5 są identyczne - pliki są takie same"
+    else
+        echo -e "\nSumy MD5 są różne - pliki się różnią"
+    fi
+}
+
+# Główna część skryptu
+clear
+echo ""
+echo "########## Dodawanie poprawek do listy słów ##########"
+
+# 1. Sprawdź czy pliki istnieją
+sprawdz_pliki
+
+# 2. Pokaż różnice
+porownaj_pliki
+
+# 3. Zapytaj o aktualizację i wykonaj ją
+aktualizuj_plik
+
+# 4. Zweryfikuj sumy kontrolne
+weryfikuj_md5
+echo ""
+echo -e "########## Operacja zakończona ##########"
+echo ""
+exit 0
